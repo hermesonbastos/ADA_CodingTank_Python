718ac4fcdd9cbe0152e20f8d38d9d96cd0158677
hermesonbastos <hermesonbastos@gmail.com>
2023-03-31 14:57:44 -0300

    piloto

---
+++ variaveis.py
@@ -0,0 +1,34 @@
+# > Variáveis
+
+idade = 19 # Criando uma variável
+
+print(idade)
+
+nome = 'Hermeson Bastos'
+
+print(nome)
+
+"""
+    Tipos de variáveis
+
+    1. int: números inteiros, ou seja, números sem parte decimal: 0, 5, -1, 1000
+    2. float: números reais, ou seja, números com parte decimal
+    3. str: cadeias de caracteres, ou seja dados textuais
+    4. bool: valores lógicos (booleanos): True ou False
+"""
+
+idade = 19
+altura = 1.74
+nome = 'Hermeson Bastos'
+estudando = True
+
+print( type(idade) )
+print( type(altura) )
+print( type(nome) )
+print( type(estudando) )
+
+
+# > Obtendo dados do usuário e salvando em variáveis
+
+linguagem = input('Qual é a linguagem de programação que você está estudando? ')
+print('A linguagem de programação que você está estudando é', linguagem)
